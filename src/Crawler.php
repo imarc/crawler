30148292fc0cdc96e9322c9ff13a3f28925340af
Chandler Blum <chandler@imarc.com>
2017-10-13 17:50:20 -0400

    fixing autoload issues

--- src/Crawler.php
+++ src/Crawler.php
@@ -1,7 +1,18 @@
 #!/usr/bin/env php
 <?php
 
-require dirname(__DIR__) . '/vendor/autoload.php';
+$paths = [
+    __DIR__ . '/../vendor/autoload.php', // locally
+    __DIR__ . '/../../../autoload.php' // dependency
+];
+
+foreach ($paths as $path) {
+    if (file_exists($path)) {
+        require_once $path;
+        break;
+    }
+}
+
 
 use Symfony\Component\Console\Application;
 use Imarc\Crawler\Commands\CsvCommand;
